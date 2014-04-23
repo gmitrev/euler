0ff653c11c4e1ae687b4571b38e7e8156d9c8905
gmitrev <gvmitrev@gmail.com>
2014-04-23 14:08:22 +0300

    Initial clojure skeleton added

---
+++ clj/project.clj
@@ -0,0 +1,6 @@
+(defproject clj "0.1.0-SNAPSHOT"
+  :description "Project Euler solutions"
+  :url "https://github.com/gmitrev/euler"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
